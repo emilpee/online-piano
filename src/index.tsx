e3b5524b8741ce8cba8e6da5ecd08a07d575994d
Emil Petersson <emilpet93@gmail.com>
2020-10-07 11:25:06 +0200

    Delete app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { Header, Piano, Footer } from './components'
+import './styles/main.scss'
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Header />
+        <Piano />
+        <Footer />
     </React.StrictMode>,
     document.getElementById('root'),
 )
