90fd213ca3879f255b5567a4b3ef67151817800f
Emil Petersson <emilpet93@gmail.com>
2020-10-07 11:25:25 +0200

    Began creating styles on piano

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -2,7 +2,44 @@ import React, { FunctionComponent } from 'react'
 import './styles.scss'
 
 const Piano: FunctionComponent = () => {
-    return <div className="piano">Piano component</div>
+    return (
+        <div className="piano">
+            <ul>
+                <li className="white c"></li>
+                <li className="black c#"></li>
+                <li className="white d"></li>
+                <li className="black d#"></li>
+                <li className="white e"></li>
+                <li className="white f"></li>
+                <li className="black f#"></li>
+                <li className="white g"></li>
+                <li className="black g#"></li>
+                <li className="white a"></li>
+                <li className="black a#"></li>
+                <li className="white b"></li>
+                <li className="white c"></li>
+                <li className="black c#"></li>
+                <li className="white d"></li>
+                <li className="black d#"></li>
+                <li className="white e"></li>
+                <li className="white f"></li>
+                <li className="black f#"></li>
+                <li className="white g"></li>
+                <li className="black g#"></li>
+                <li className="white a"></li>
+                <li className="black a#"></li>
+                <li className="white b"></li>
+                <li className="white c"></li>
+                <li className="black c#"></li>
+                <li className="white d"></li>
+                <li className="black d#"></li>
+                <li className="white e"></li>
+                <li className="white f"></li>
+                <li className="black f#"></li>
+                <li className="white g"></li>
+            </ul>
+        </div>
+    )
 }
 
 export default Piano
