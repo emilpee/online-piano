d081c147a90ea214a0207c76d0d804427806da47
Emil <emilpet93@gmail.com>
2020-10-10 18:32:02 +0200

    Correct note plays when clicking on piano key

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -1,52 +1,46 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useEffect, useState } from 'react'
 import { Howl, Howler } from 'howler'
+import pianoData from './data/pianoData.json'
+import PianoKeysContainer from './components/PianoKeysContainer'
+import PianoKey from './components/PianoKey'
+import PianoVolume from './components/PianoVolume'
 import './styles.scss'
 
 const Piano: FunctionComponent = () => {
+    const [volume, setVolume] = useState<number>(1)
 
-    const playNote = () => {
+    useEffect(() => {
+        Howler.volume(Math.round(volume * 10) / 10)
+    }, [volume])
+
+    const playNote = (event: React.MouseEvent<HTMLElement>): void => {
+        event.persist()
+
+        let pianoNote = (event.target as Element).id
         let sound = new Howl({
-            src: ['/audio/c3.mp3'],
-          })
+            src: [`/audio/${pianoNote}.mp3`],
+        })
+            
         sound.play()
+    }
+
+    const handlePianoVolume = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const buttonType = event.target as HTMLButtonElement
+
+        if (buttonType.id === 'inc' && volume < 1) {
+            setVolume(volume + 0.1)
+        } else if (buttonType.id === 'dec' && volume > 0.1) {
+            setVolume(volume - 0.1)
+        }
     }
 
     return (
         <div className="piano">
-            <ul>
-                <li onClick={playNote} className="white c"></li>
-                <li className="black c#"></li>
-                <li className="white d"></li>
-                <li className="black d#"></li>
-                <li className="white e"></li>
-                <li className="white f"></li>
-                <li className="black f#"></li>
-                <li className="white g"></li>
-                <li className="black g#"></li>
-                <li className="white a"></li>
-                <li className="black a#"></li>
-                <li className="white b"></li>
-                <li className="white c"></li>
-                <li className="black c#"></li>
-                <li className="white d"></li>
-                <li className="black d#"></li>
-                <li className="white e"></li>
-                <li className="white f"></li>
-                <li className="black f#"></li>
-                <li className="white g"></li>
-                <li className="black g#"></li>
-                <li className="white a"></li>
-                <li className="black a#"></li>
-                <li className="white b"></li>
-                <li className="white c"></li>
-                <li className="black c#"></li>
-                <li className="white d"></li>
-                <li className="black d#"></li>
-                <li className="white e"></li>
-                <li className="white f"></li>
-                <li className="black f#"></li>
-                <li className="white g"></li>
-            </ul>
+            <PianoKeysContainer>
+                <PianoVolume id="inc" handlePianoVolume={handlePianoVolume}>+</PianoVolume>
+                <PianoVolume id="dec" handlePianoVolume={handlePianoVolume}>-</PianoVolume>
+                {pianoData.map((pianoKey) => <PianoKey onKeyClick={playNote} key={pianoKey.key} id={pianoKey.key} className={`piano-key ${pianoKey.type}`} />)}
+            </PianoKeysContainer>
         </div>
     )
 }
