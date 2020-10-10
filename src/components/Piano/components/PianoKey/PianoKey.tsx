441473809c4d8283bca694630976bb29f0e1c816
Emil <emilpet93@gmail.com>
2020-10-10 18:31:16 +0200

    Created more piano components

---
+++ src/components/Piano/components/PianoKey/PianoKey.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react'
+
+interface PianoKeyProps { 
+    id: string
+    onKeyClick: (event: React.MouseEvent<HTMLElement>) => void
+    className?: string
+}
+
+const PianoKey: FunctionComponent<PianoKeyProps> = (props) => {
+    const { onKeyClick, className, id } = props
+
+    return (
+        <li onClick={onKeyClick} id={id} className={className}></li>
+    )
+}
+
+export default PianoKey
---
+++ src/components/Piano/components/PianoVolume/PianoVolume.tsx
@@ -0,0 +1,19 @@
+import React, { FunctionComponent } from 'react'
+
+interface PianoVolumeProps {
+    id: string
+    handlePianoVolume: (event: React.MouseEvent<HTMLButtonElement>) => void
+    children?: React.ReactNode | React.ReactText
+}
+
+const PianoVolume: FunctionComponent<PianoVolumeProps> = (props) => {
+    const { handlePianoVolume, children, id } = props
+
+    return (
+        <div className="piano-volume">
+            <button className="piano-volume-button" id={id} onClick={handlePianoVolume}>{children}</button>
+        </div>
+    )
+}
+
+export default PianoVolume
