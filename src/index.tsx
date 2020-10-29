45b49dfeffc617848a55841abab81c4f77083e38
Emil <emilpet93@gmail.com>
2020-10-29 19:52:57 +0100

    Create Mui theme colors with provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import { ThemeProvider } from '@material-ui/core'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Header, Piano, Footer } from './components'
 import './styles/main.scss'
+import { theme } from './styles/theme'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Header />
-    <Piano />
-    <Footer />
+    <ThemeProvider theme={theme}>
+      <Header />
+      <Piano />
+      <Footer />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+    palette: {
+      primary: {
+          main: '#0cfc03'
+      },
+      secondary: {
+          main: '#FC030C'
+      }
+    },
+});
