5634baa235d6110ab31e7fe1a0d1742a9e96e25a
Emil <emilpet93@gmail.com>
2020-10-29 22:49:27 +0100

    Global styles defined in mui theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,28 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 
 export const theme = createMuiTheme({
+    overrides: {
+        MuiCssBaseline: {
+          '@global': {
+            '#root': {
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100vh',
+            },
+            body: {
+                background: '#eee',
+                fontFamily: 'Nunito, Arial, sans-serif',
+                fontWeight: 400,
+                margin: 0,
+                padding: 0,
+                WebkitFontSmoothing: 'auto',
+            },
+            span: {
+                color: '#fff'
+            }
+          },
+        },
+      },
     palette: {
       primary: {
           main: '#0cfc03'
